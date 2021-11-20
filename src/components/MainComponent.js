5c5200b543d12b7cdb2886af26f11907ea96e496
Emily Butler <emilybutler382@gmail.com>
2021-11-20 10:31:58 -0600

    react router parameters

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Contact from './ContactComponent';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
 import { PROMOTIONS } from '../shared/promotions';
+import CampsiteInfo from './CampsiteInfoComponent';
 
 class Main extends Component {
     constructor(props) {
@@ -31,6 +32,17 @@ class Main extends Component {
                 />
             );
         };
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            )
+        }
+
+
         return (
             <div>
                 <Header />
@@ -38,6 +50,7 @@ class Main extends Component {
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
                     <Route exact path='/contactus' component={Contact} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Redirect to='/home' />
                 </Switch>
                 <Footer />
